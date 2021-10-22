739ca1cc6a77b77e8eea5ac0d1e8761a83651bec
hotratx <mgviero@gmail.com>
2021-10-22 19:48:12 -0300

    antes do alembic, tudo funcionando

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+# pull official base image
+FROM python:3.9-slim-buster
+
+# set working directory
+WORKDIR /usr/src/app
+
+# set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+# install system dependencies
+RUN apt-get update \
+  && apt-get -y install netcat gcc postgresql curl\
+  && apt-get clean
+
+# Install Poetry
+RUN curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | POETRY_HOME=/opt/poetry python && \
+    cd /usr/local/bin && \
+    ln -s /opt/poetry/bin/poetry && \
+    poetry config virtualenvs.create false
+
+# Copy poetry.lock* in case it doesn't exist in the repo
+COPY ./pyproject.toml ./poetry.lock* /usr/src/app/
+
+# Looks like poetry fails to add itself to the Path in Docker. We add it here.
+ARG INSTALL_DEV=false
+RUN bash -c "if [ $INSTALL_DEV == 'true' ] ; then poetry install --no-root ; else poetry install --no-root --no-dev ; fi"
+
+# add app
+COPY . .
---
+++ app/main.py
@@ -0,0 +1,34 @@
+from fastapi import Depends, FastAPI
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db import get_session
+from app.models import Song, SongCreate
+from app.db import init_db
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def on_startup():
+    await init_db()
+
+@app.get("/ping")
+async def pong():
+    return {"ping": "pong!"}
+
+
+@app.get("/songs", response_model=list[Song])
+async def get_songs(session: AsyncSession = Depends(get_session)):
+    result = await session.execute(select(Song))
+    songs = result.scalars().all()
+    return [Song(name=song.name, artist=song.artist, year=song.year, id=song.id) for song in songs]
+
+
+@app.post("/songs")
+async def add_song(song: SongCreate, session: AsyncSession = Depends(get_session)):
+    song_db = Song(name=song.name, artist=song.artist, year=song.year)
+    session.add(song_db)
+    await session.commit()
+    await session.refresh(song_db)
+    return song_db
