fdeba4351aee40dc87c6111440db164496673202
hotratx <mgviero@gmail.com>
2021-10-25 10:17:03 -0300

    refactor testing, crud, api

--- app/api/crud/songs.py
+++ app/api/crud/songs.py
@@ -1,7 +1,6 @@
 from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.db import get_session
 from app.api.models.models import Song, SongCreate
 
 
@@ -21,24 +20,3 @@ class SongDB():
         songs = resp.scalars().all()
         #return [Song(name=song.name, artist=song.artist, year=song.year, id=song.id) for song in songs]
         return songs
-
-import asyncio
-
-async def song_post(db: AsyncSession, song: SongCreate):
-    song_db = Song(name=song.name, artist=song.artist, year=song.year)
-    db.add(song_db)
-    await db.commit()
-    await db.refresh(song_db)
-    return song_db
-
-
-async def song_get(session):
-    #resp = await db.execute(select(Song))
-    #songs = resp.scalars().all()
-    asyncio.sleep(3)
-    songs = {'name': 'amim'}
-    #return [Song(name=song.name, artist=song.artist, year=song.year, id=song.id) for song in songs]
-    return songs
-
-
-
--- tests/test_ping.py
+++ tests/test_ping.py
@@ -1,22 +1,32 @@
-from starlette.testclient import TestClient
 import json
-import pytest
 
-from app.api.crud import songs
+from app.api.crud.songs import SongDB
 from app.api.models.models import Song, SongCreate
 
 
-def test_ping(test_app, monkeypatch):
+def test_post_song(test_app, monkeypatch):
     test_request_payload = {'name': 'amim', 'artist': 'eu', 'year': 19}
-    test_response_payload = {"id": 3,'name': 'amim', 'artist': 'eu', 'year': 19}
 
-    async def mock_post(db, song):
+    async def mock_post(db, session):
         return test_request_payload
 
 
-    monkeypatch.setattr(songs, "song_post", mock_post)
+    monkeypatch.setattr(SongDB, "create", mock_post)
 
     response = test_app.post("/songs", data=json.dumps(test_request_payload),)
 
-    assert response.status_code == 200
+    assert response.status_code == 201
     assert response.json() == test_request_payload
+
+def test_get_song(test_app, monkeypatch):
+    test_response_payload = {"id": 3,'name': 'amim', 'artist': 'eu', 'year': 19}
+
+    async def mock_get(session):
+        return test_response_payload
+
+    monkeypatch.setattr(SongDB, "get", mock_get)
+
+    response = test_app.get("/songs")
+
+    assert response.status_code == 200
+    assert response.json() == test_response_payload
